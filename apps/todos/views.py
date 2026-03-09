f0a6979706af1aa5787fc99982708938fbdc0537
emon51 <emanur11235@gmail.com>
2026-03-09 15:24:43 +0600

    refactor: update todos app to use generic views

--- apps/todos/views.py
+++ apps/todos/views.py
@@ -1,18 +1,13 @@
-from rest_framework import status
+from rest_framework import generics
 from rest_framework.permissions import AllowAny
-from rest_framework.request import Request
-from rest_framework.response import Response
-from rest_framework.views import APIView
 
 from .models import Todo
 from .serializers import TodoSerializer
 
 
-class TodoListCreateView(APIView):
-    permission_classes = [AllowAny]  
+class TodoListCreateView(generics.ListCreateAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+    permission_classes = [AllowAny]
 
-    def post(self, request: Request) -> Response:
-        serializer = TodoSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
