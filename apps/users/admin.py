a9988e3f14a7fd95cb2a4b53f9c06e210f6dbbcc
emon51 <emanur11235@gmail.com>
2026-03-12 13:06:55 +0600

    register User and Todo models in admin

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "email", "is_active", "created_at"]
+    search_fields = ["name", "email"]
+    ordering = ["-created_at"]
